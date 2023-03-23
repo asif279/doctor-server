03fb04fb048091c49ec20433dc6caf03fb509f07
asif279 <asifofficial8130@gmail.com>
2023-03-23 23:48:34 +0600

    a

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const express = require('express')
-const cors = require('cors')
+
+const cors = require('cors');
+require('dotenv').config()
 const app = express()
 app.use(cors())
 app.use(express.json())
+
 const port = 5000
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gdmv2qb.mongodb.net/?retryWrites=true&w=majority`;
+
+
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+    const appointment=client.db('doctor').collection('treatment');
+    app.get('/treatment',async(req,res)=>{
+        const query={};
+        const option =await appointment.find(query).toArray();
+        res.send(option);
+
+    })
+
+  }
+  finally{
+
+  }
+}
+run().catch(console.log);
+
 
 app.get('/', async(req, res) => {
   res.send('Hello World!')
